00735bbe115c8cca6faa795d0e41878a508531c3
mki578 <megankateirving@gmail.com>
2014-08-24 18:28:05 +1000

    lab3
    adding the third lab to a different branch - hopefully!!

--- android/Yo2/src/com/example/Yo2/MyActivity.java
+++ android/Yo2/src/com/example/Yo2/MyActivity.java
@@ -1,15 +1,19 @@
 package com.example.Yo2;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import org.w3c.dom.Text;
 
 import java.util.Random;
 
@@ -28,14 +32,45 @@ public class MyActivity extends Activity
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+       //setContentView(R.layout.main);
 
         // connect elements to code
-        messageLabel = (TextView) findViewById(R.id.label);
-        yoText = (EditText) findViewById(R.id.yo);
-        messageText = (EditText) findViewById(R.id.name);
-        doStuff = (Button) findViewById(R.id.button);
-        layout = (LinearLayout) findViewById(R.id.screen);
+        //messageLabel = (TextView) findViewById(R.id.label);
+        //yoText = (EditText) findViewById(R.id.yo);
+        //messageText = (EditText) findViewById(R.id.name);
+        //doStuff = (Button) findViewById(R.id.button);
+        //layout = (LinearLayout) findViewById(R.id.screen);
+
+        // create label
+        messageLabel = new TextView(this);
+        messageLabel.setText("Yo \nYour Name");
+        messageLabel.setTextSize(40);
+        messageLabel.setGravity(Gravity.CENTER);
+        // create text editors
+        yoText = new EditText(this);
+        yoText.setText("Yo");
+        messageText = new EditText(this);
+        messageText.setText("Your Name");
+        // create button
+        doStuff = new Button(this);
+        doStuff.setText("Show Message");
+        // create params
+        LinearLayout.LayoutParams center = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT, 0.9f);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        // create linear layout
+        layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        // set params
+        layout.setLayoutParams(params);
+        messageLabel.setLayoutParams(center);
+        // layout.setGravity(Gravity.CENTER);
+        layout.addView(yoText);
+        layout.addView(messageText);
+        layout.addView(messageLabel);
+        layout.addView(doStuff);
+        layout.setBackgroundColor(Color.RED);
+
+        setContentView(layout);
 
         // the button's listener
         doStuff.setOnClickListener(new View.OnClickListener()
@@ -75,7 +110,6 @@ public class MyActivity extends Activity
         layout.setBackgroundColor(color);
 
         // updates text
-        messageLabel.setAllCaps(true);
         String msg = yoText.getText() + "\n" + messageText.getText();
         messageLabel.setText(msg);
     }
