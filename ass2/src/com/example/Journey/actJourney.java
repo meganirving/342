f9591576213a15c393bdb2bcf09a326212071196
mki578 <megankateirving@gmail.com>
2014-09-30 13:53:47 +1000

    Revert "Revert "adding a code skeleton""
    This reverts commit ddb1a268fcd83f42ed43eb286e871c52aa5c5768.

---
+++ ass2/src/com/example/Journey/actJourney.java
@@ -0,0 +1,15 @@
+package com.example.Journey;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class actJourney extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
