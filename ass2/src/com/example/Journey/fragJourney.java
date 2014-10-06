ad405a8210e6ea5f719ec5e37bc0331da80a009b
mki578 <megankateirving@gmail.com>
2014-10-06 16:35:57 +1100

    recording a journey WIP

--- ass2/src/com/example/Journey/fragJourney.java
+++ ass2/src/com/example/Journey/fragJourney.java
@@ -1,17 +1,173 @@
 package com.example.Journey;
 
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 /**
  * Created by Megan on 29/9/2014.
  */
-public class fragJourney extends Fragment {
+public class fragJourney extends Fragment implements View.OnClickListener {
+
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+    private Uri fileUri;
+    private View root;
+    private Button btnRec;
+    private Button btnCam;
+    private Button btnStop;
+    private GoogleMap map;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.journey, container, false);
+        root = inflater.inflate(R.layout.journey, container, false);
+
+        btnRec = (Button) root.findViewById(R.id.butRecord);
+        btnRec.setOnClickListener(this);
+        btnCam = (Button) root.findViewById(R.id.butCamera);
+        btnCam.setOnClickListener(this);
+        btnStop = (Button) root.findViewById(R.id.butStop);
+        btnStop.setOnClickListener(this);
+
+        // TODO: put a map in the map frame
+
+        return root;
+    }
+
+    // called when the record button is pressed
+    public void RecordButton(View v) {
+        // hide this button
+        v.setVisibility(View.GONE);
+
+        // show the other two buttons
+        btnCam.setVisibility(View.VISIBLE);
+        btnStop.setVisibility(View.VISIBLE);
+
+        // display toast
+        Context context = getActivity().getApplicationContext();
+        CharSequence text = "Recording...";
+        int duration = Toast.LENGTH_SHORT;
+        Toast.makeText(context, text, duration).show();
+
+        // TODO: start collecting GPS information to put in the journey
+    }
+
+    // called when the camera button is pressed
+    public void CameraButton(View v) {
+        /*
+         // create Intent to take a picture and return control to the calling application
+    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+    fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE); // create a file to save the image
+    intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set the image file name
+
+    // start the image capture Intent
+    startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+         */
+    }
+
+    // when the camera intent returns
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        /*
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // Image captured and saved to fileUri specified in the Intent
+                Toast.makeText(getActivity(), "Image saved to:\n" +
+                        data.getData(), Toast.LENGTH_LONG).show();
+
+                 // TODO: display an alertdialog where the user can fill in an optional comment
+                 // TODO: keep track of the image + comment
+
+            } else if (resultCode == RESULT_CANCELED) {
+                // User cancelled the image capture
+            } else {
+                // Image capture failed, advise user
+            }
+        }*/
+    }
+
+    // called when the stop button is pressed
+    public void StopButton(View v) {
+
+        SaveJourney("Untitled");
+
+        // create an alertdialog for getting the journey name
+        AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
+
+        // add a message and a textbox
+        alert.setMessage("Give your journey a title:");
+        final EditText input = new EditText(getActivity());
+        alert.setView(input);
+
+        // add buttons
+        alert.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked OK button, so call the save function
+                final String title = input.getText().toString();
+                // if the user entered a title
+                if (!title.isEmpty()){
+                    SaveJourney(title);
+                } else {
+                    // otherwise save it with a default title
+                    SaveJourney("Untitled Journey");
+                }
+
+            }
+        });
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User cancelled the dialog, do nothing
+            }
+        });
+
+        // Create the AlertDialog
+        AlertDialog dialog = alert.create();
+    }
+
+    // saves the journey to database, resets button visibility, and displays a toast
+    public void SaveJourney(String title) {
+        // hide the stop and camera buttons
+        btnStop.setVisibility(View.GONE);
+        btnCam.setVisibility(View.GONE);
+
+        // show the record button
+        btnRec.setVisibility(View.VISIBLE);
+
+        // TODO: save the whole journey to the database
+
+        // create toast
+        Context context = getActivity().getApplicationContext();
+        CharSequence text = "Journey saved!";
+        int duration = Toast.LENGTH_SHORT;
+
+        // show toast
+        Toast.makeText(context, text, duration).show();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.butStop:
+                StopButton(v);
+                break;
+            case R.id.butCamera:
+                CameraButton(v);
+                break;
+            case R.id.butRecord:
+                RecordButton(v);
+                break;
+        }
     }
 }
