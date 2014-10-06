a94cdd0be3d8db160a0b9f5deeeaf2b7b8202414
mki578 <megankateirving@gmail.com>
2014-10-06 21:50:27 +1100

    starting the list
    setting up the list adapter

--- ass2/src/com/example/Journey/fragList.java
+++ ass2/src/com/example/Journey/fragList.java
@@ -17,9 +17,35 @@ import java.util.List;
  */
 public class fragList extends Fragment {
 
+    private MySQLHelper mySql;
+    private ArrayList<tblJourney> journeys;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.list, container, false);
+
+        // get the list frame
+        ListView list = (ListView) root.findViewById(R.id.list);
+
+        // get all journeys
+        mySql = new MySQLHelper(getActivity());
+        mySql.open();
+        journeys = mySql.getAllJourneys();
+
+        // create the adapter
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, android.R.id.text1, journeys) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TextView text1 = (TextView) view.findViewById(android.R.id.text1);
+
+                text1.setText(journeys.get(position).getTitle());
+                return view;
+            }
+        };
+
+        // attach the adapter to the frame
+        list.setAdapter(adapter);
 
         return root;
     }
